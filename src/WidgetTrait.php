6db57e560f512d28346f71b1f94439dfe56cd19b
Duncan de Boer <duncan.d.boer@gmail.com>
2021-11-28 22:06:50 +0100

    initial commit

---
+++ src/WidgetTrait.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dsdeboer\GoogleTagManager;
+
+use Bolt\Widget\Injector\RequestZone;
+
+trait WidgetTrait
+{
+    /**
+     * Gets an extension config value.
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    private function getConfig(string $key)
+    {
+        return $this->extension->getConfig()->get($key);
+    }
+
+    /**
+     * Determines whether the widget is
+     *
+     * @return bool
+     */
+    private function isEnabled()
+    {
+        $zones = $this->getConfig('zones');
+
+        // if zones are not set, then assume it shouldn't be shown anywhere.
+        if (!is_array($zones) || count($zones) === 0) {
+            return false;
+        }
+
+        $request = $this->extension->getRequest();
+
+        foreach ($zones as $zone => $isEnabled) {
+            if (!$isEnabled) {
+                continue;
+            }
+
+            // Config only accepts backend and frontend, not async.
+            if (!in_array($zone, [RequestZone::FRONTEND, RequestZone::BACKEND], true)) {
+                continue;
+            }
+
+            if (!RequestZone::is($request, $zone)) {
+                continue;
+            }
+
+            // We're in the right zone and it's enabled in config.
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return string|null
+     */
+    private function build(): ?string
+    {
+        $googleTagManagerId = $this->getConfig('google_tag_manager');
+
+        if (empty($googleTagManagerId)) {
+            return null;
+        }
+
+        if ($this->isEnabled()) {
+            return parent::run(['google_tag_manager' => $googleTagManagerId]);
+        }
+
+        return null;
+    }
+}
